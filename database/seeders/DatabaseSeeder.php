c34ca92168c62cefbc8b9b68ffd26466f72b96f3
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-07-05 11:18:50 -0400

    supression du modele role pour utilisateur

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        $this->call(RolesTableSeeder::class);
         $this->call(TypesTableSeeder::class);
     }
 }
