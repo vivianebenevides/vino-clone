643dff9bdc5801ef32e3f7fcdd58d80b24ada380
vivianebenevides <vivianebenevides.ca@gmail.com>
2023-07-13 12:00:41 -0400

    Retification detail-boutteille

--- resources/views/bouteilles/details.blade.php
+++ resources/views/bouteilles/details.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 @section('titre', 'DETAILS')
 @section('content')
-        <main>
-            <!--  faut ajouter le html de la page -->
-           l id de la bouteils :  {{$bouteille->id}}
+<main>
+            <section class="container">
+            </section>
+
+            <section class="detail">
+                <div class="detail-text">
+                    <p>Vin rouge</p>
+                    <h2>EA Cartuxa Reserva</h2>
+                    <p>Vin rouge | 750 ml | Portugal</p>
+                    <p>Code produit : 12345678</p>
+                </div>
+                <div class="detail-img">
+                    <img src="assets/vin-rouge.png" alt="bouteille">
+                </div>
+                <div class="btn-carte">
+                    <a href="#">29.95 $</a>
+                    <a href="#">Ajouter à mon celier</a>
+                </div>
+            </section>
         </main>
 @endsection
